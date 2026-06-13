53b6934745eae210d6e6033cf15ede85dd34dbb2
ziad396 <tayelzeiad17@gmail.com>
2026-06-14 01:14:48 +0300

    updated

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,34 @@
 <?php
 
+// use Illuminate\Auth\AuthenticationException;
+// use Illuminate\Foundation\Application;
+// use Illuminate\Foundation\Configuration\Exceptions;
+// use Illuminate\Foundation\Configuration\Middleware;
+
+// return Application::configure(basePath: dirname(__DIR__))
+//     ->withRouting(
+//         web: __DIR__.'/../routes/web.php',
+//         api: __DIR__.'/../routes/api.php',
+//         commands: __DIR__.'/../routes/console.php',
+//         health: '/up',
+//     )
+//      ->withMiddleware(function ($middleware) {
+//     $middleware->alias([
+//         'user' => \App\Http\Middleware\TaskMiddleware::class,
+//         'api' => \App\Http\Middleware\ApiMiddleware::class,
+        
+//     ]);
+  
+//     })
+//     ->withMiddleware(function (Middleware $middleware): void {
+
+//     })
+//     ->withExceptions(function (Exceptions $exceptions): void {
+//         //
+//     })->create();
+    
+
+
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
@@ -12,17 +41,18 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-     ->withMiddleware(function ($middleware) {
-    $middleware->alias([
-        'user' => \App\Http\Middleware\TaskMiddleware::class,
-        'api' => \App\Http\Middleware\ApiMiddleware::class,
-        
-    ]);
-  
-    })
-    ->withMiddleware(function (Middleware $middleware): void {
-
+    ->withMiddleware(function ($middleware) {
+        $middleware->alias([
+            'user' => \App\Http\Middleware\TaskMiddleware::class,
+            'api' => \App\Http\Middleware\ApiMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->render(function (AuthenticationException $e, \Illuminate\Http\Request $request) {
+            return response()->json([
+                'message' => 'Unauthorized - User must be logged in',
+                'success' => false,
+                'error' => 'authentication_required'
+            ], 401);
+        });
     })->create();
